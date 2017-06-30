81e0620b8413ab7f37a28b25355d60e6bec104c4
avorochenko <vorochenko@gmail.com>
2017-06-30 14:59:34 +0300

    implemented likes

--- index.android.js
+++ index.android.js
@@ -5,7 +5,7 @@ import {
     ActivityIndicator, Alert, ListView,
     StyleSheet, RefreshControl, TextInput,
     AppRegistry, TouchableHighlight, Button,
-    Text, View, Image, ScrollView
+    Text, View, Image, AsyncStorage
 } from 'react-native';
 
 const BASE_URL = 'https://api.instagram.com/v1';
@@ -41,22 +41,41 @@ class HomeScreen extends React.Component {
             isOpen: false,
             selectedItem: 'About',
             refreshing: false,
-            likes:0,
+            likes:{},
             dataSource: this.getDs([])
         }
     }
 
     onLike = (id) => {
+        let currentObject = this.state.likes;
+        let countLikes = currentObject[id] !== undefined
+            ? currentObject[id] + 1
+            : 1;
+        currentObject[id] = countLikes;
         this.setState({
-            likes: this.state.likes + 1
-        })
+            likes: currentObject
+        });
+
+        AsyncStorage.setItem('INSTA_LIKES', JSON.stringify(currentObject));
+        AsyncStorage.getItem('INSTA_LIKES', (err, result) => {
+            console.log(result);
+        });
     }
 
-    _onRefresh(tagName) {
-
+    _onRefresh = (tagName)=> {
         this.setState({refreshing: true});
         this.fetchImages(tagName);
         this.setState({refreshing: false});
+
+        AsyncStorage.getItem('INSTA_LIKES', (err, result) => {
+            console.log('result', result);
+
+            this.setState({
+                likes: result
+            });
+
+        }).then(()=>{console.log('likes', this.state.likes);
+            });
     }
 
     getDs(data){
@@ -69,7 +88,7 @@ class HomeScreen extends React.Component {
 
     fetchImages =(tagName)=>{
         getMedia(tagName).then((response) => {
-            console.log(response);
+
             this.setState({
                 isLoading: false,
                 dataSource: this.getDs(response)
@@ -139,10 +158,9 @@ class HomeScreen extends React.Component {
                                     onPress={()=> {this.onLike(rowData.caption.id)}}/>
                           </View>
                           <Text >
-                              {this.state.likes}
+                              {this.state.likes[rowData.caption.id] }
                           </Text>
                         </View>
-
 
                         <Text>{rowData.caption.id}</Text>
                         <Text>{rowData.caption.text}</Text>
