2b8e1facd6b094044a54c7a184aabf1c145f1082
olbor <olbor13@gmail.com>
2017-07-07 18:00:01 +0300

    Custom header on AboutScreen

--- app/AboutScreen.js
+++ app/AboutScreen.js
@@ -35,17 +35,39 @@ export default class AboutScreen extends React.Component {
             </FillLayout>*/
 
             <View>
+
                 <View style={{flex: 0, height: 50, flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-start', backgroundColor: 'orange',}}>
-                    <View style={{ marginLeft: 10, marginRight: 90 }}>
+                    <TouchableHighlight
+                        style={{ marginLeft: 10, marginRight: 120 }}
+                        onPress={Actions.pop}>
+                        <Image
+                            source={{uri:'https://d30y9cdsu7xlg0.cloudfront.net/png/344376-200.png'}}
+                            style={styles.icon}
+                        />
+                    </TouchableHighlight>
+
+                    {/*<View style={{ marginLeft: 10, marginRight: 90 }}>
                         <Button onPress={Actions.pop} title='Back' />
-                    </View>
+                    </View>*/}
+
                     <View style={{ marginLeft: 20, marginRight: 20 }}>
                         <Button onPress={Actions.homeScreen} title='Home' />
                     </View>
-                    <View style={{ marginLeft: 90, marginRight: 10}}>
+
+                    {/*<View style={{ marginLeft: 90, marginRight: 10}}>
                         <Button onPress={Actions.menuScreen} title='Menu' />
-                    </View>
+                    </View>*/}
+
+                    <TouchableHighlight
+                        style={{ marginLeft: 120, marginRight: 10}}
+                        onPress={Actions.menuScreen}>
+                        <Image
+                            source={{uri:'https://cdn3.iconfinder.com/data/icons/32-fufficon/32/32x32_fluffy-03-512.png'}}
+                            style={styles.icon}
+                        />
+                    </TouchableHighlight>
                 </View>
+
 
                 <View style={{margin: 128}}>
                     <Text style={{padding: 10}}>This is the best ever application!</Text>
